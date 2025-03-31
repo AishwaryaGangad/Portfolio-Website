a3e401449e666e34bcee633ccdaa1fcd25e47dfe
Prathmesh3177 <prathmeshbondar@gmail.com>
2025-03-31 18:53:32 +0530

    Update project configuration and files

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -18,7 +18,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ImMap } from "react-icons/im";
+//import { ImMap } from "react-icons/im";
 
 function App() {
   const [load, upadateLoad] = useState(true);
